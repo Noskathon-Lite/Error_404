447291f1986c72587794464d766696f1d6bcd30a
darshan2436 <thapadarshan447@gmail.com>
2025-01-12 09:21:34 +0545

    Resource page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import Home from './Home';
+import Resource from './Resource';
 import MoodTracker from './MoodTracker';
 import { BrowserRouter as Router , Routes ,  Route , Link } from 'react-router-dom';
 
@@ -10,11 +11,13 @@ function App() {
           <ul className='flex m-2'>
             <li className='m-2'><Link to="/">Home</Link></li>
             <li className='m-2'><Link to="/mood-tracker">Mood Tracker</Link></li>
+            <li className='m-2'><Link to="/resource">Resource</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mood-tracker" element={<MoodTracker />} />
+          <Route path="/resource" element={<Resource />} />
         </Routes>
       </Router>
     </div>
