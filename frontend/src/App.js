1f1ed18dbd7191cb3dfad9ccab3e0355437052d4
nishanstha74 <shrestha.nishan060@gamil.com>
2025-01-12 14:10:26 +0545

    add registerform and import it  in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Home from './Home';
 import Resource from './Resource';
 import MoodTracker from './MoodTracker';
 import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 import { SearchBar } from './component/SearchBar';
 import { CategorySection } from './component/CategorySection';
 // import { fetchResources } from './api/resources';
@@ -44,6 +45,8 @@ function App() {
           <Route path="/search-bar" element={<SearchBar />} />
           <Route path="/category-section" element={<CategorySection />} />
           <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          
         </Routes>
       </Router>
     </div>
