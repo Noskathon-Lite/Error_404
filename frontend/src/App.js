f4f6ca541dd0ba80b2e80a0a2568abb0fa64a844
Hari Shankar Rana <ranahari836@gmail.com>
2025-01-12 12:47:05 +0545

    resource library updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import Home from './Home';
 import Resource from './Resource';
 import MoodTracker from './MoodTracker';
-import { BrowserRouter as Router , Routes ,  Route , Link } from 'react-router-dom';
+import { SearchBar } from './component/SearchBar';
+import { CategorySection } from './component/CategorySection';
+// import { fetchResources } from './api/resources';
+import { 
+  AlertCircle, 
+  Loader, 
+  Heart,
+  Brain,
+  Cloud,
+  Frown,
+  Users,
+  Sparkles,
+  Menu
+} from 'lucide-react';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -12,12 +27,16 @@ function App() {
             <li className='m-2'><Link to="/">Home</Link></li>
             <li className='m-2'><Link to="/mood-tracker">Mood Tracker</Link></li>
             <li className='m-2'><Link to="/resource">Resource</Link></li>
+            <li className='m-2'><Link to="/search-bar">SearchBar</Link></li>
+            <li className='m-2'><Link to="/category-section">CategorySection</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mood-tracker" element={<MoodTracker />} />
           <Route path="/resource" element={<Resource />} />
+          <Route path="/search-bar" element={<SearchBar />} />
+          <Route path="/category-section" element={<CategorySection />} />
         </Routes>
       </Router>
     </div>
